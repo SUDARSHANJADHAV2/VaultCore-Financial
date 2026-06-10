9b377e9f8bd76d6ae1bb24d736428ecdb9535f3b
SUDARSHAN SANTAJI JADHAV <sudarshanjadhav2@zohomail.in>
2026-06-10 22:54:24 +0530

    Create AuditLogRepository.java

---
+++ vaultcore_backend/src/main/java/com/vaultcore/repository/AuditLogRepository.java
@@ -0,0 +1,15 @@
+package com.vaultcore.repository;
+
+import com.vaultcore.model.AuditLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
+
+    List<AuditLog> findAllByOrderByCreatedAtDesc();
+
+    List<AuditLog> findTop50ByUsernameOrderByCreatedAtDesc(String username);
+}

4af220ba4e9c5c068b2259d50c391cd3a4f43132
SUDARSHAN SANTAJI JADHAV <sudarshanjadhav2@zohomail.in>
2026-06-12 22:54:48 +0530

    Create VaultcoreApplicationTests.java

---
+++ vaultcore_backend/src/test/java/com/vaultcore/VaultcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaultcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

e9fbc71a056b7fe38299305c7fa9a2ea4855527b
SUDARSHAN SANTAJI JADHAV <sudarshanjadhav2@zohomail.in>
2026-06-12 22:54:54 +0530

    Create CustomUserDetailsService.java

---
+++ vaultcore_backend/src/main/java/com/vaultcore/service/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.vaultcore.service;
+
+import com.vaultcore.model.User;
+import com.vaultcore.repository.UserRepository;
+import com.vaultcore.security.CustomUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        return new CustomUserDetails(user);
+    }
+}
